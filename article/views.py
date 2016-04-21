57c0d4fd1eca7111538cd60236be88a1800c4414
727987568@qq.com <727987568@qq.com>
2016-04-21 21:14:40 +0800

    my first view

--- article/views.py
+++ article/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Article
 # Create your views here.
+
+
+def index(request):
+    article_list = Article.objects.order_by('-pub_date')[:5]
+    context = {'list_article': article_list}
+    return render(request, 'article/index.html', context)
+
+
+def detail(request, article_id):
+    return HttpResponse("you are look at article %s" %article_id)
+
